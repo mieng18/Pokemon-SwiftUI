dc7a9d3c394b890b1958e33cb8ab1730a89f8e66
mieng18 <mieng18@hotmail.com>
2023-07-13 14:05:49 -0700

    Initial Commit

---
+++ PokemonApp/PokemonAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonAppApp.swift
+//  PokemonApp
+//
+//  Created by mai nguyen on 7/13/23.
+//
+
+import SwiftUI
+
+@main
+struct PokemonAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
